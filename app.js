121cc7f43a38c37779bbd172994ced9929f12524
Claudia <clau1205dgzvzla@gmail.com>
2023-06-11 08:21:48 -0600

    first commit

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
 //404.html -Vista
 //input.html -referencia
 //index.html - vista - views
+
+//vamos a usar las rutas definidas en routes
 //assets en public
 const express = require('express')
 const hbs = require('hbs')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const puerto = process.env.PORT || 3000;
+const rutasCategorias = require('./src/routes/categorias-routes-api');
 
 const app = express();
 //Vistas dinamicas
@@ -21,12 +24,16 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// RUTAS para los datos - API - END POINTS 
+//Vamos a usar las rutas definidas en routes
+app.use(rutasCategorias);
+//RUTAS PARA LAS VISTAS
 //Rutas - temporales
 
 app.get('/',(req,res)=>{
     res.render('dashboard');
 })
-
+//Esta ruta ya funciona
 app.get('/categorias',(req,res)=>{
     res.render('categorias');
 })
